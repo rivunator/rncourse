1fc4f41a2e7200291e07e445ab0797fc0079640a
Rivo Seim <rivo@neocard.fi>
2019-04-21 14:47:09 +0300

    removed onpress for view

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -5,11 +5,7 @@ import ListItem from "../ListItem/ListItem";
 
 const placeList = props => {
   const placesOutput = props.places.map((place, i) => (
-    <ListItem
-      key={i}
-      placeName={place}
-      onItemPressed={() => props.onItemDeleted(i)}
-    />
+    <ListItem key={i} placeName={place} />
   ));
   return <View style={styles.listContainer}>{placesOutput}</View>;
 };
